78d0cd4c901535edd11978a5a4d72abfad5dfac3
ameriken <hitohahito@hotmail.co.jp>
2020-05-09 11:23:53 +0900

    競技プログラミング用base作成

---
+++ ABC/for.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+for i in {2..100}
+do
+    i=`printf %03d $i`
+    echo ${i}
+	mkdir -p abc${i}
+	cp -r  a.cpp abc${i}/a.cpp
+	cp -r  b.cpp abc${i}/b.cpp
+	cp -r  c.cpp abc${i}/c.cpp
+	cp -r  d.cpp abc${i}/d.cpp
+done
