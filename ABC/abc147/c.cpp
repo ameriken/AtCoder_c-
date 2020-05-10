8790a9b097ab54e7cdc667d1045dc99a4ea0a1cc
ameriken <hitohahito@hotmail.co.jp>
2020-05-11 00:19:21 +0900

    ケンちょんさんのコードに変更

--- ABC/abc147/c.cpp
+++ ABC/abc147/c.cpp
@@ -1,42 +1,65 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// 1 つの証言を表す構造体
+using pint = pair<int,int>; // 「人」と「0 or 1」
+
+// 入力
+int N;
+vector<vector<pint>> v;
+
+// 整数 bit に対応する「決め打ち」が整合しているかを判定
+bool judge(int bit) {
+
+    // i 人目の証言を検証する
+    for (int i = 0; i < N; ++i) {
+
+        // i 人目が「不親切」だったら、証言はすべて無意味
+        if ( !(bit & (1 << i)) ) continue;
+
+        // それぞれ確認
+        for (pint xy : v[i]) {
+            int x = xy.first; // x が
+            int y = xy.second; // y = 1: 正直、y = 0: 不親切
+
+            // y = 1 なのに「不親切」だったらダメ
+            if (y == 1 && !(bit & (1 << x))) return false;
+
+            // y = 0 なのに「正直者」だったらダメ
+            if (y == 0 && (bit & (1 << x))) return false;
+        }
+    }
+    return true;
+}
+
+
 int main() {
-
-    int N;
     cin >> N;
-
-    vector<int> A(N);
-    vector<vector<int> > x(N), y(N);
-
-    // ここで初期化をしている
-    // x[i][j] は親切な人か不親切な人か
-    // y[i][j] は真実か嘘か
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-        x[i] = vector<int>(A[i]);
-        y[i] = vector<int>(A[i]);
-        for (int j = 0; j < A[i]; j++) {
-            cin >> x[i][j] >> y[i][j];
-            x[i][j]--;
+    v.resize(N);
+    for (int i = 0; i < N; ++i) {
+        int A; cin >> A;
+        v[i].resize(A);
+        for (int j = 0; j < A; ++j) {
+            cin >> v[i][j].first >> v[i][j].second;
+            --v[i][j].first; // 0-indexed に
         }
     }
 
-    int ans = 0;
-    for (int bit = 0; bit < (1 << N); bit++) {
-        int cnt = 0;
-        bool good = true;
-        for (int i = 0; i < N; i++) {
-            if (bit & (1 << i)) {
-                cnt++;
-                for (int j = 0; j < A[i]; j++) {
-                    if (((bit >> x[i][j]) & 1) != y[i][j]) {
-                        good = false;
-                    }
-                }
+    int res = 0;
+    for (int bit = 0; bit < (1 << N); ++bit) {
+
+        // 矛盾 check
+        if (judge(bit)) {
+
+            // bit に含まれる人数を数える
+            int count = 0;
+            for (int i = 0; i < N; ++i) {
+                if (bit & (1 << i)) ++count;
             }
+            res = max(res, count);
         }
-        if (good) ans = max(ans, cnt);
     }
-    cout << ans << endl;
+
+    cout << res << endl;
 }
